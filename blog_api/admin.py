b54fde5221d7b23bde83e3e1e0e6c2eeedb627c9
yadav himanshu8000 <104956836+yadavhimanshu8000@users.noreply.github.com>
2025-06-16 10:13:40 +0530

    Update admin.py

--- blog_api/admin.py
+++ blog_api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 
+
 # Register your models here.
+
+admin.site.register(Countries)
+admin.site.register(User)
+admin.site.register(Merchants)
+admin.site.register(Order)
+admin.site.register(order_item)
+admin.site.register(products)
+
 
 
